159358b01dba66dab95e678578169f19994b45db
Moises David <mdlb.lobo@gmail.com>
2019-08-27 11:50:50 -0500

    Añadido el control para la clase Fibonacci

--- Practica 2/index.php
+++ Practica 2/index.php
@@ -7,6 +7,10 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <form action="Control.php" method="post">
+    <label for="num1">Ingrese Limite de la serie:</label>
+    <input type="text" name="num1" id="num1">
+    <input type="submit" value="Enviar">
+  </form>
 </body>
 </html>
