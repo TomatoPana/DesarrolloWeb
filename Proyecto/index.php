31e439829707ac5189dcdcdcc719a2129f90f0bc
Moises David <mdlb.lobo@gmail.com>
2019-09-05 13:26:36 -0500

    Añadido clase Calculadora de la practica 1

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -1,1 +1,32 @@
 <?php
+class Calculadora {
+    protected $numero1;
+    protected $numero2;
+    protected $resultado;
+    protected $operador;
+    public function setNumero1($numero1){
+        $this->numero1 = $numero1;
+    }
+    public function setNumero2($numero2){
+        $this->numero2 = $numero2;
+    }
+    public function setOperador($operador){
+        switch($operador){
+            case '+':
+                $this->resultado = $this->numero1 + $this->numero2;
+            break;
+            case '-':
+                $this->resultado = $this->numero1 - $this->numero2;
+            break;
+            case '*':
+                $this->resultado = $this->numero1 * $this->numero2;
+            break;
+            case '/':
+                $this->resultado = $this->numero1 / $this->numero2;
+            break;
+        }
+    }
+    public function getResultado(){
+        return $this->resultado;
+    }
+}
