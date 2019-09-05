18a2b7bb06c7a4393af95efda9d6e68efb46b008
Moises David <mdlb.lobo@gmail.com>
2019-09-05 13:55:52 -0500

    Añadida todas las operaciones (No las conversiones)

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -1,4 +1,5 @@
 <?php
+//TODO ^,%(Porcentaje no Módulo),!,^2,sin,cos,tan,sqrt,DecToBin,BintoDec,DecToOct,OctToDec,DecToHex,HexToDec
 class Calculadora {
     protected $numero1;
     protected $numero2;
@@ -25,6 +26,32 @@ class Calculadora {
                 $this->resultado = $this->numero1 / $this->numero2;
             break;
         }
+    }
+    public function square(){
+      $this->resultado = $this->numero1 * $this->numero1;
+    }
+    public function porcentaje(){
+      $this->resultado = $this->numero1 * ($this->numero2 / 100);
+    }
+    public function pot(){
+      $this->resultado = $this->numero1 ** $this->numero1;
+    }
+    public function factorial(){
+      if($this->numero1 > 0){
+        $this->resultado = 1;
+        for ($i=1; $i <= $this->numero1; $i++) { 
+          $this->resultado = $this->resultado*$i;
+        }
+      }
+    }
+    public function sin(){
+      $this->resultado = sin($this->numero1);
+    }
+    public function cos(){
+      $this->resultado = cos($this->numero1);
+    }
+    public function tan(){
+      $this->resultado = tan($this->numero1);
     }
     public function getResultado(){
         return $this->resultado;
